8c473cd6d08580994253d315552ea2a5983db64a
Mohamed Mourad <s-mohamed.mmagdy@zewailcity.edu.eg>
2024-11-23 16:33:26 +0200

    added needed constructor overload

--- Patient.h
+++ Patient.h
@@ -16,8 +16,8 @@ private:
     char patient_type[2]; // Values range from only {SP, EP, NP} 
     
 public:
-    Patient(/* args */);                    // should handle the patient ID automatically
-    // should also set the request time
+    Patient();
+    Patient(char P_type[2], int request_time, int patient_ID, int requested_hospital, int distance);
     void set_type(char P_type[2]);
     void request(int time);
     void set_distance(int distance);
@@ -29,7 +29,25 @@ public:
     void set_assign_time(int timestep, int car_speed); //should also calculate all other times
     void set_request_time(int timestep); 
     // Geters for each data member____________________________________________________
+    int get_request_time() const { return request_time; }
 
+    int get_pick_time() const { return pick_time; }
+
+    int get_dist() const { return dist; }
+
+    int get_wait_time() const { return wait_time; }
+
+    int get_finish_time() const { return finish_time; }
+
+    int get_case_severity() const { return case_severity; }
+
+    int get_cancel_time() const { return cancel_time; }
+
+    int get_hospitalID() const { return hospitalID; }
+
+    int get_patientID() const { return patientID; }
+
+    string get_patient_type() const { return patient_type; }
 };
 
 Patient::Patient(/* args */)
