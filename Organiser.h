2eb63473abc3fba0d11c1e614cbc1aff019c81f2
Mohamed Mourad <s-mohamed.mmagdy@zewailcity.edu.eg>
2024-11-23 23:52:25 +0200

    done: simulation logic remaining: program interface

--- Organiser.h
+++ Organiser.h
@@ -17,8 +17,11 @@ private:
     LinkedQueue<Patient*> finished_requests;
 public:
     Organiser();
-    void load_file(string file_name);
-
+    void load_file(string file_name, int& hopitals_num, int& requests_num);
+    bool move_to_finish(int hospitalID, string request_type, int& finished_num);
+    bool assign_car(int hospitalID, char car_type, int current_time);
+    bool carry_back(int current_time);
+    bool backTo_hospital();
     ~Organiser();
 };
 
@@ -29,7 +32,7 @@ Organiser::Organiser(){
     finished_requests=LinkedQueue<Patient*>();
 }
 
-void Organiser::load_file(string file_name){
+void Organiser::load_file(string file_name, int& hopitals_num, int& requests_num){
     int normal_speed, special_speed;
     ifstream test(file_name);
     test >> numOf_hospitals >> special_speed >> normal_speed;
@@ -37,17 +40,50 @@ void Organiser::load_file(string file_name){
     for(int i{0}; i < numOf_hospitals; i++){
         int special_cars, normal_cars;
         test>>special_cars>>normal_cars;
-        hospitals[i].set_cars(normal_speed, special_speed, special_cars, normal_cars);
+        hospitals[i].set_cars(normal_speed, special_speed, special_cars, normal_cars, i);
     }
-    int numOfRequest; test>>numOfRequest;
+    int numOf_requests; test>>numOf_requests;
     string patient_type; int request_time, PID, HID, dist, severity;
-    while (numOfRequest--){
+    while (numOf_requests--){
         test >> patient_type>>request_time>>PID>>HID>>dist;
         Patient* new_request = new Patient(patient_type, request_time, PID, HID, dist);
         if(patient_type=="EP"){test>>severity; new_request->set_case_severity(severity);}
         hospitals[HID].add_request(new_request, patient_type);
     }
     test.close();
+    hopitals_num = numOf_hospitals ;
+    requests_num = numOf_requests;
+}
+
+bool Organiser::move_to_finish(int hospitalID, string request_type, int& finished_num){
+    Patient* p = hospitals[hospitalID].remove_request(request_type);
+    if(!p){return false;}
+    if(finished_requests.enqueue(p)){finished_num ++;return true;}
+    return false;
+}
+
+bool Organiser::assign_car(int hospitalID, char car_type, int current_time){
+    Car* c = hospitals[hospitalID].remove_available_car(car_type);
+    if(!c){return false;}
+    out_cars.enqueue(c, current_time);
+    return true;
+}
+
+bool Organiser::carry_back(int current_time){
+    Car* back{nullptr}; int assign_time;
+    if(out_cars.dequeue(back, assign_time)){
+        back_cars.enqueue(back, current_time); return true;
+    }
+    return false;
+}
+
+bool Organiser::backTo_hospital(){
+    Car* c = nullptr;int p;
+    if(back_cars.dequeue(c,p)){
+        hospitals[c->get_owning_hospital()].add_free_car(c, c->get_type());
+        return true;
+    }
+    return false;
 }
 
 Organiser::~Organiser(){
